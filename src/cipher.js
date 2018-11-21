f1752fb9fe943f67a9e1ff767bfa8beccaa98dbd
MirellaLucero <lucero_200900@hotmail.com>
2018-11-21 13:00:33 -0500

    Solucionado el problema de límites,agregando boton de volver

--- src/cipher.js
+++ src/cipher.js
@@ -2,8 +2,9 @@ const output1=document.getElementById("output1");
 function crypt(data1,key1) {
  var data1= document.getElementById("data1").value;
  var key1=parseInt(document.getElementById("key1").value);
+ var key1= key1%26;
  var upperWords = data1.toUpperCase();
- var alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split ("");
+ var alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
  var textCripted="";
 
  for(let i=0; i<upperWords.length; i++){
@@ -15,8 +16,12 @@ continue;
 
 var currentIndex= alphabet.indexOf(letter);
 var newIndex= currentIndex+key1;
-if(newIndex>25) newIndex=newIndex-26;
-if(newIndex<0) newIndex=newIndex+26;
+if(newIndex>25 ) {
+  newIndex=newIndex-26;
+}
+if(newIndex<0) {
+  newIndex=newIndex+26;
+}
 if (data1[i]===data1[i].toLowerCase()) {
  textCripted+=alphabet[newIndex].toLowerCase();
 }
@@ -38,8 +43,9 @@ const output2=document.getElementById("output2");
 function crypt2(data2,key2) {
  var data2= document.getElementById("data2").value;
  var key2=parseInt(document.getElementById("key2").value);
+ var key2=key2%26
  var upperWords = data2.toUpperCase();
- var alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split ("");
+ var alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
  var textCripted="";
 
  for(let i=0; i<upperWords.length; i++){
@@ -51,8 +57,12 @@ continue;
 
 var currentIndex= alphabet.indexOf(letter);
 var newIndex= currentIndex-key2;
-if(newIndex>25) newIndex=newIndex-26;
-if(newIndex<0) newIndex=newIndex+26;
+if(newIndex>25) {
+  newIndex=newIndex-26;
+}
+if(newIndex<0) {
+  newIndex=newIndex+26;
+}
 if (data2[i]===data2[i].toLowerCase()) {
  textCripted+=alphabet[newIndex].toLowerCase();
 }
--- src/index.js
+++ src/index.js
@@ -10,5 +10,19 @@ function enviar2() {
  document.getElementById("pagdescifrar").style.display='block';
 }
 
+function volver1() {
+  event.preventDefault();
+  document.getElementById("pagcifrar").style.display='none';
+  document.getElementById("inicio").style.display='block';
+}
+
+function volver2() {
+  event.preventDefault();
+  document.getElementById("pagdescifrar").style.display='none';
+  document.getElementById("inicio").style.display='block';
+}
+
 document.getElementById("paseCe").addEventListener("click",enviar1);
 document.getElementById("paseDe").addEventListener("click",enviar2);
+document.getElementById("volver1").addEventListener("click",volver1);
+document.getElementById("volver2").addEventListener("click",volver2);
