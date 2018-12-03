61e4bc58055c19951e653851d25aed2ad2e79826
MirellaLucero <lucero_200900@hotmail.com>
2018-12-03 08:26:34 -0500

    cambios en cipher tests

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,18 @@
-
-//función
 window.cipher={
   encode: (offset,string) => {
   const mensaje= string;//almacenando el valor de data en la const mensaje
   let clave=parseInt(offset);//almacenando el valor de key en clave
  const upperWords = string.toUpperCase();//conviertiendo en mayúsculas
- const alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789¡!¿?,.;-_#$%&/()=:><";//declarando abecedario
+ const alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";//declarando abecedario
  let textEncode="";//almacen del texto encriptado
  for(let i=0; i<upperWords.length; i++){
    let letter= upperWords[i];
  if(letter === " "){textEncode+=letter;
 continue;}
 let currentIndex= alphabet.indexOf(letter);
-let newIndex= (currentIndex+clave)%55;
-if(newIndex>54 ) {newIndex=newIndex-55;}
-if(newIndex<0) {newIndex=newIndex+55;}
+let newIndex= (currentIndex+clave)%26;
+if(newIndex>25 ) {newIndex=newIndex-26;}
+if(newIndex<0) {newIndex=newIndex+26;}
 if (mensaje[i]===mensaje[i].toLowerCase()) {
  textEncode+=alphabet[newIndex].toLowerCase();
 }
@@ -28,7 +26,7 @@ decode: (offset,string) => {
   const mensaje= string;//almacenando el valor de data en la const mensaje
   let clave=parseInt(offset);//almacenando el valor de key en clave
  const upperWords = string.toUpperCase();//conviertiendo en mayúsculas
- const alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789¡!¿?,.;-_#$%&/()=:><";//declarando abecedario
+ const alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";//declarando abecedario
  let textDecode="";//almacen del texto desencriptado
  for(let i=0; i<upperWords.length; i++){
    let letter= upperWords[i];//almacenando la letra actual en la var letter
@@ -37,9 +35,9 @@ decode: (offset,string) => {
 continue;
 }
 let currentIndex= alphabet.indexOf(letter);//obteniendo ubicación en el alfabeto de la letra y asignandole el valor a actualvalor
-let newIndex= (currentIndex-clave)%55;//descifrando, restando la clave a la ubicación actual de la letra
-if(newIndex>54) {newIndex=newIndex-55;}
-if(newIndex<0) {newIndex=newIndex+55;}//para que me devuelva en minúsculas si es que lo ingresan en minúsculas
+let newIndex= (currentIndex-clave)%26;//descifrando, restando la clave a la ubicación actual de la letra
+if(newIndex>25) {newIndex=newIndex-26;}
+if(newIndex<0) {newIndex=newIndex+26;}//para que me devuelva en minúsculas si es que lo ingresan en minúsculas
 if (mensaje[i]===mensaje[i].toLowerCase()) {textDecode+=alphabet[newIndex].toLowerCase();}
 //almacene en la palabra final en la var textDecode
 else textDecode+=alphabet[newIndex];
